c4ec242d3a100f1f34a974e717b31ec07e08eba3
akhiltheguitarist <akhiltheguitarist@gmail.com>
2021-04-11 03:06:55 +0530

    first component changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import SimpleCard from '../src/components/card-layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{'display':'flex'}}>
+      <div style={{'display':'flex', 'flexDirection':'column'}}>
+        <SimpleCard/>
+        <SimpleCard/>
+      </div>
+      <SimpleCard/>
+      </div>
     </div>
   );
 }
